9cf8eb6f8f54ce21fd1fc3267505f6e478d489ed
najmus-sakib-hossain <manfromexistence@proton.me>
2025-06-06 12:27:14 +0000

    fix: update placeholder text from "Lists are coming..." to "Contents are coming..."

--- www/app/[lang]/contents/page.tsx
+++ www/app/[lang]/contents/page.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export default function Contents() {
     return (
         <div className="container flex h-screen flex-col items-center justify-center space-y-8 pb-[75px]">
-            <span className="text-3xl font-bold md:text-4xl lg:text-5xl xl:text-6xl">Lists are coming...</span>
+            <span className="text-3xl font-bold md:text-4xl lg:text-5xl xl:text-6xl">Contents are coming...</span>
         </div>
     );
 }
