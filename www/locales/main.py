6c68559c9fac461e9e1e2b7693fa1b1b7c26d004
najmus-sakib-hossain <manfromexistence@proton.me>
2025-06-06 09:41:35 +0000

    feat: localizations updated

--- www/locales/main.py
+++ www/locales/main.py
@@ -5,12 +5,12 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 # List of language codes
 language_codes = [
-    # "af","sq","am","ar","hy","as","ay","az","bm","eu",
-    # "be","bn","bho","bs","bg","ca","ceb","ny","zh-CN","zh-TW",
-    # "co","hr","cs","da","dv","doi","nl","en","eo","et",
-    # "ee","tl","fi","fr","fy","gl","ka","de","el","gn",
-    # "gu","ht","ha","haw","iw","hi","hmn","hu",
-    # "is","ig","ilo","id","ga","it","ja",
+    "af","sq","am","ar","hy","as","ay","az","bm","eu",
+    "be","bn","bho","bs","bg","ca","ceb","ny","zh-CN","zh-TW",
+    "co","hr","cs","da","dv","doi","nl","en","eo","et",
+    "ee","tl","fi","fr","fy","gl","ka","de","el","gn",
+    "gu","ht","ha","haw","iw","hi","hmn","hu",
+    "is","ig","ilo","id","ga","it","ja",
     "jw","kn","kk","km","rw","gom","ko","kri","ku","ckb",
     "ky","lo","la","lv","ln","lt",
     "lg","lb","mk","mai","mg","ms","ml","mt","mi","mr","mni-Mtei","lus","mn","my","ne","no","or","om","ps","fa","pl","pt","pa",
